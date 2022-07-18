f9cb3a2939e8bb162558f08a82c3f1207237497f
Anna <anna47297@gmail.com>
2022-07-18 15:23:12 +0300

    feat: add js file

---
+++ youmap/js/scroll.js
@@ -0,0 +1,23 @@
+
+
+// document.querySelector('.egg').onclick = function () {
+//     this.style.transform = `rotate(${this.d = (this.d | 0) + 90}deg)`;
+//   }
+
+
+// const phone = document.querySelector('.right_part_img');
+
+// window.addEventListener('scroll', animated);
+
+// function animated(){
+//     let posTop = window.pageYOffset;
+//     if(posTop == 0 || posTop < 300){
+//         console.log('dfdf');
+//         phone.style.transform = "translate(235%, -114px)";
+//     } else if (posTop > 301) {
+//         console.log('работает');
+//         phone.style.transform = `translate(235%, ${posTop+800}px)`
+//     }
+//     console.log(posTop);
+   
+// };
