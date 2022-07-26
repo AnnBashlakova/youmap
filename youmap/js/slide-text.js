c65c1a29934b9f943925028d22657c32f6cbfb18
Anna <anna47297@gmail.com>
2022-07-26 21:00:34 +0300

    feat: add animation block

--- youmap/js/slide-text.js
+++ youmap/js/slide-text.js
@@ -27,7 +27,7 @@ const phone = document.querySelector('.right_part_img')
 window.addEventListener('scroll', scrollPhone);
 function scrollPhone() {
     let vertical_position = pageYOffset;
-    console.log(vertical_position);
+    // console.log(vertical_position);
     if(vertical_position > 2510){
         console.log('dfvdfdf');
         phone.classList.remove('stick');
@@ -40,12 +40,4 @@ function scrollPhone() {
     
         // phone.classList.add('pos');
     }
-    // imgPhone.style.marginTop = (vertical_position + 100) + 'px';
-    // if(vertical_position > 1250){
-    //   // console.log('finish')
-    //   imgPhone.style.marginTop = (vertical_position + 100) + 'px';
-    //   // your_div.style.marginTop = (vertical_position-100) + 'px';
-    // } else if(vertical_position == 2060) {
-    //   imgPhone.style.marginTop = (vertical_position - 200) + 'px';
-    // }
 };
